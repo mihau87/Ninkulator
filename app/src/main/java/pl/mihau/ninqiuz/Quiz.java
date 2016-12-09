c92c966dba97cf67d6dbd90fb21fd9e11c9b9928
Michał Adamczyk <michal.adamczyk@medapp.pl>
2016-12-09 22:36:24 +0100

    nowe funkcje Ninquiz

--- app/src/main/java/pl/mihau/ninqiuz/Quiz.java
+++ app/src/main/java/pl/mihau/ninqiuz/Quiz.java
@@ -2,9 +2,12 @@ package pl.mihau.ninqiuz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class Quiz extends AppCompatActivity {
 
@@ -26,6 +29,7 @@ public class Quiz extends AppCompatActivity {
     TextView pointsTextView;
     int life = 3;
     int points = 0;
+    int startFlag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,5 +68,197 @@ public class Quiz extends AppCompatActivity {
         lifeTextView.setVisibility(View.INVISIBLE);
         pointsTextView.setVisibility(View.INVISIBLE);
         answerTextView.setVisibility(View.INVISIBLE);
-}
-}
+
+
+        checkButton.setOnClickListener (new View.OnClickListener()
+                                        {
+                                            @Override
+                                            public void onClick(View view)
+                                            {
+
+        if (startFlag == 0){
+
+            checkButton.setText("Sprawdź");
+            button0.setVisibility(View.VISIBLE);
+            button1.setVisibility(View.VISIBLE);
+            button2.setVisibility(View.VISIBLE);
+            button3.setVisibility(View.VISIBLE);
+            button4.setVisibility(View.VISIBLE);
+            button5.setVisibility(View.VISIBLE);
+            button6.setVisibility(View.VISIBLE);
+            button7.setVisibility(View.VISIBLE);
+            button8.setVisibility(View.VISIBLE);
+            button9.setVisibility(View.VISIBLE);
+            clearButton.setVisibility(View.VISIBLE);
+            clearButton.setEnabled(false);
+            lifeTextView.setVisibility(View.VISIBLE);
+            pointsTextView.setVisibility(View.VISIBLE);
+            answerTextView.setVisibility(View.VISIBLE);
+
+           // randomEquation();
+            int goodAnswer = 21;
+            do{
+                Random r = new Random();
+                int firstNum = r.nextInt(20 + 1);
+                int secndNum = r.nextInt(20 + 1);
+
+                goodAnswer = firstNum + secndNum;
+
+                questionTextView.setText(firstNum + " + " + secndNum + " = ?");
+
+        }while(goodAnswer > 20 || goodAnswer < 0);
+        }
+        else {
+            return;
+        }                                   }
+                                        }
+        );
+
+        clearButton.setOnClickListener (new View.OnClickListener()
+                                        {
+                                            @Override
+                                            public void onClick(View view)
+                                            {
+
+                                                answerTextView.setText("");
+                                            }
+                                        }
+        );
+
+        button0.setOnClickListener (new View.OnClickListener()
+                                    {
+                                        @Override
+                                        public void onClick(View view){
+
+                                            answerTextView.append((button0.getText().toString()));
+
+                                        }
+                                    }
+        );
+
+        button1.setOnClickListener (new View.OnClickListener()
+                                    {
+                                        @Override
+                                        public void onClick(View view){
+
+                                            answerTextView.append((button1.getText().toString()));
+
+                                        }
+                                    }
+        );
+
+        button2.setOnClickListener (new View.OnClickListener()
+                                    {
+                                        @Override
+                                        public void onClick(View view){
+                                            answerTextView.append((button2.getText().toString()));
+
+                                        }
+                                    }
+        );
+
+        button3.setOnClickListener (new View.OnClickListener()
+                                    {
+                                        @Override
+                                        public void onClick(View view){
+                                            answerTextView.append((button3.getText().toString()));
+
+                                        }
+                                    }
+        );
+        button4.setOnClickListener (new View.OnClickListener()
+                                    {
+                                        @Override
+                                        public void onClick(View view){
+                                            answerTextView.append((button4.getText().toString()));
+
+                                        }
+                                    }
+        );
+        button5.setOnClickListener (new View.OnClickListener()
+                                    {
+                                        @Override
+                                        public void onClick(View view){
+                                            answerTextView.append((button5.getText().toString()));
+
+                                        }
+                                    }
+        );
+        button6.setOnClickListener (new View.OnClickListener()
+                                    {
+                                        @Override
+                                        public void onClick(View view){
+                                            answerTextView.append((button6.getText().toString()));
+                                        }
+                                    }
+        );
+        button7.setOnClickListener (new View.OnClickListener()
+                                    {
+                                        @Override
+                                        public void onClick(View view){
+                                            answerTextView.append((button7.getText().toString()));
+                                        }
+                                    }
+        );
+        button8.setOnClickListener (new View.OnClickListener()
+                                    {
+                                        @Override
+                                        public void onClick(View view){
+                                            answerTextView.append((button8.getText().toString()));
+
+                                        }
+                                    }
+        );
+        button9.setOnClickListener (new View.OnClickListener()
+                                    {
+                                        @Override
+                                        public void onClick(View view){
+                                            answerTextView.append((button9.getText().toString()));
+                                        }
+                                    }
+        );
+
+
+
+//        public boolean checkWin(int currentPoint)
+//        {
+//            if (currentPoint == 10)
+//            {
+//               return true;
+//            }
+//            else
+//            {
+//               return false;
+//            }
+//        };
+//
+//    public boolean checkLoose(int currentLife)
+//    {
+//        if (currentLife == 0)
+//        {
+//            return true;
+//        }
+//        else
+//        {
+//            return false;
+//        }
+//    };
+
+//        public int randomEquation()
+//        {
+//            int goodQuestion = 0;
+//            while(goodQuestion > 20 || goodQuestion < 0) {
+//                Random r = new Random();
+//                int firstNum = r.nextInt(20 + 1);
+//                int secndNum = r.nextInt(20 + 1);
+//
+//                goodQuestion = firstNum + secndNum;
+//            }
+//            return goodQuestion;
+//        };
+
+    }
+
+
+    }
+
